21bd12edd05f6ef9941679a0896feb2f6c388dda
JOSE HERMES GONZALEZ VASQUEZ <jose.gonzalez210723@liveusam.edu.sv>
2025-03-26 22:45:46 -0600

    Agregar archivos de proyecto.

---
+++ MVCusingCoreDBFirst/Models/CompanyContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCusingEFCoreDBFirst.Models
+{
+    public partial class CompanyContext : DbContext
+    {
+        public CompanyContext()
+        {
+        }
+
+        public CompanyContext(DbContextOptions<CompanyContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("your_connection_string_here");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC07E4671ABD");
+                entity.ToTable("Employee");
+                entity.Property(e => e.Department).HasMaxLength(50);
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+                entity.Property(e => e.LastName).HasMaxLength(50);
+                entity.Property(e => e.Salary).HasColumnType("numeric(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ MVCusingCoreDBFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCusingCoreDBFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
